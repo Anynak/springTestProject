410345bf7572aad3a924d5dd52b9912a3a6b46f9
Anynak <anynak69@gmail.com>
2020-11-05 18:29:52 +0300

    work

--- src/main/java/testgroup/filmography/controller/FilmController.java
+++ src/main/java/testgroup/filmography/controller/FilmController.java
@@ -5,16 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import testgroup.filmography.model.Film;
+import testgroup.filmography.service.FilmService;
+import testgroup.filmography.service.FilmServiceImpl;
+
+import java.util.List;
 
 @Controller
 public class FilmController {
-
+    private FilmService filmService = new FilmServiceImpl();
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView allFilms() {
         ModelAndView modelAndView = new ModelAndView();
+        List<Film> films = filmService.allFilms();
         modelAndView.setViewName("films");
-        modelAndView.addObject("film", film);
-        modelAndView.addObject("film2", film);
+        modelAndView.addObject("filmsList", films);
+
         return modelAndView;
     }
 
@@ -25,14 +30,6 @@ public class FilmController {
         return modelAndView;
     }
 
-    private static Film film;
 
-    static {
-        film = new Film();
-        film.setTitle("Inception");
-        film.setYear(2010);
-        film.setGenre("sci-fi000");
-        film.setWatched(true);
-    }
 
 }
--- src/main/webapp/pages/films.jsp
+++ src/main/webapp/pages/films.jsp
@@ -1,10 +1,33 @@
-<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
-    <title>Title</title>>
+    <title>FILMS</title>
 </head>
 <body>
-    ${film}
-    <h2><a href="${pageContext.request.contextPath}/edit">edit page</a> </h2>
+
+<h2>Films</h2>
+<table>
+    <tr>
+        <th>id</th>
+        <th>title</th>
+        <th>year</th>
+        <th>genre</th>
+        <th>watched</th>
+        <th>action</th>
+    </tr>
+    <c:forEach var="film" items="${filmsList}">
+        <tr>
+            <td>${film.id}</td>
+            <td>${film.title}</td>
+            <td>${film.year}</td>
+            <td>${film.genre}</td>
+            <td>${film.watched}</td>
+            <td>
+                <a href="/edit/${film.id}">edit</a>
+            </td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
