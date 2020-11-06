86faf5cd13694b25d2fe626188f118815a02adb9
Anynak <anynak69@gmail.com>
2020-11-06 18:24:00 +0300

    work

--- src/main/java/testgroup/filmography/controller/FilmController.java
+++ src/main/java/testgroup/filmography/controller/FilmController.java
@@ -1,26 +1,44 @@
 package testgroup.filmography.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import testgroup.filmography.model.Film;
 import testgroup.filmography.service.FilmService;
 import testgroup.filmography.service.FilmServiceImpl;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
 public class FilmController {
     private FilmService filmService = new FilmServiceImpl();
+
+    @RequestMapping
+    public String handleAllTradesRequests (@RequestHeader("User-Agent") String userAgent,
+                                           Model model) {
+        model.addAttribute("msg", "all trades requests, User-Agent header  : "
+                + userAgent);
+        System.out.println(userAgent);
+
+        return "my-page";
+    }
+
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView allFilms() {
+    public String allFilms(HttpServletRequest request) {
+
+        String userAgent = request.getHeader("User-Agent");
         ModelAndView modelAndView = new ModelAndView();
         List<Film> films = filmService.allFilms();
-        modelAndView.setViewName("films");
-        modelAndView.addObject("filmsList", films);
+        //modelAndView.setViewName("films");
+        modelAndView.addObject("filmsList", userAgent.toString());
 
-        return modelAndView;
+        return "films";
     }
 
     @RequestMapping(value = "/edit", method = RequestMethod.GET)
--- src/main/webapp/pages/films.jsp
+++ src/main/webapp/pages/films.jsp
@@ -7,27 +7,13 @@
 <body>
 
 <h2>Films</h2>
+<h3>"${filmsList}"</h3>
 <table>
     <tr>
         <th>id</th>
-        <th>title</th>
-        <th>year</th>
-        <th>genre</th>
-        <th>watched</th>
-        <th>action</th>
+
     </tr>
-    <c:forEach var="film" items="${filmsList}">
-        <tr>
-            <td>${film.id}</td>
-            <td>${film.title}</td>
-            <td>${film.year}</td>
-            <td>${film.genre}</td>
-            <td>${film.watched}</td>
-            <td>
-                <a href="/edit/${film.id}">edit</a>
-            </td>
-        </tr>
-    </c:forEach>
+
 </table>
 </body>
 </html>
